3f7223f4f573c6d3f8008273c8a1b9acb6dd8720
José Tomás Ahumada <jtahumada@miuandes.cl>
2020-05-07 09:48:58 -0400

    first advance

---
+++ Ejemplo Realizacion/Ejemplo Realizacion/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Realizacion
+{
+    class Persona
+    {
+        private string name;
+        private string lastname;
+        private int age;
+        public Persona(string name, string lastname, int age)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.age = age;
+        }
+        public string Name()
+        {
+            return name;
+        }
+        public string Lastname()
+        {
+            return lastname;
+        }
+        public int Age()
+        {
+            return age;
+        }
+    }
+}
--- Ejemplo Realizacion/Ejemplo Realizacion/Program.cs
+++ Ejemplo Realizacion/Ejemplo Realizacion/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Ejemplo_Realizacion
 {
@@ -10,6 +11,7 @@ namespace Ejemplo_Realizacion
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
