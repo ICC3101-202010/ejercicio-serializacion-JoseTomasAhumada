ed11b289057c27c2c57b9dda9f0f7cb1fc32bc7d
José Tomás Ahumada <jtahumada@miuandes.cl>
2020-05-07 10:29:27 -0400

    second advance

--- Ejemplo Realizacion/Ejemplo Realizacion/Persona.cs
+++ Ejemplo Realizacion/Ejemplo Realizacion/Persona.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
 
 namespace Ejemplo_Realizacion
 {
-    class Persona
+    [Serializable]
+    class Persona : Stream, IFormatter
     {
         private string name;
         private string lastname;
@@ -28,6 +32,20 @@ namespace Ejemplo_Realizacion
         public int Age()
         {
             return age;
+        }
+        public static void SavePersons(List<Persona> listperson)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, listperson);
+            stream.Close();
+        }
+        public static void LoadPersons()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Persona person = (Persona)formatter.Deserialize(stream);
+            stream.Close();
         }
     }
 }
--- Ejemplo Realizacion/Ejemplo Realizacion/Program.cs
+++ Ejemplo Realizacion/Ejemplo Realizacion/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Ejemplo_Realizacion
 {
@@ -11,7 +12,50 @@ namespace Ejemplo_Realizacion
     {
         static void Main(string[] args)
         {
-
+            List<Persona> listperson = new List<Persona>();
+            while (true)
+            {
+                Console.WriteLine("¿Qué desea hacer a continuación? [1]Crear Persona [2]Ver Personas [3]Cargar Personas [4]Guardar Personas [5]Salir");
+                string option = Console.ReadLine();
+                if (option == "1")
+                {
+                    Console.WriteLine("Nombre de la persona:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Apellido de la persona:");
+                    string lastname = Console.ReadLine();
+                    Console.WriteLine("Edad de la persona:");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    Persona person = new Persona(name, lastname, age);
+                    listperson.Add(person);
+                }
+                if (option == "2")
+                {
+                    if (listperson.Count() == 0)
+                    {
+                        Console.WriteLine("ERROR, no hay personas registradas.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NOMBRE APELLIDO EDAD");
+                        foreach (Persona person in listperson)
+                        {
+                            Console.WriteLine(person.Name() + " " + person.Lastname() + " " + person.Age());
+                        }
+                    }
+                }
+                if (option == "3")
+                {
+                    Persona.LoadPersons();
+                }
+                if (option == "4")
+                {
+                    Persona.SavePersons(listperson);
+                }
+                if (option == "5")
+                {
+                    break;
+                }
+            }
         }
     }
 }
